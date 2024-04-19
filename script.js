54c3a46edfea64ed1ec4e45861b48fd12ca43461
gabrielcabrera17 <gabrielbaez023@gmail.com>
2024-04-19 02:25:29 -0400

    likes multiples

---
+++ script.js
@@ -0,0 +1,20 @@
+function aumentarLike(){
+    let aumentoLike =document.querySelector('.aumentoLikesNeil');
+    let actualizarAumento = Number(aumentoLike.textContent);
+    console.log("soy yi"+aumentoLike);
+    actualizarAumento++;
+    aumentoLike.textContent = actualizarAumento;
+}
+function aumentarLike2(){
+    let aumentoLike =document.querySelector('.aumentoLikesNichole');
+    let actualizarAumento = Number(aumentoLike.textContent);
+    console.log("soy yi"+aumentoLike);
+    actualizarAumento++;
+    aumentoLike.textContent = actualizarAumento;
+}
+function aumentarLike3(){
+    let aumentarLike = document.querySelector('.aumentoLikesJim');
+    let actualizarAumento = Number(aumentarLike.textContent);
+    actualizarAumento++;
+    aumentarLike.textContent = actualizarAumento;
+}
